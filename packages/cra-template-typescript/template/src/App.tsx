5b3959a0af724c395409423f8d4c8c0fa193deea
Un7corn <sjd77777djs@gmail.com>
2026-03-12 12:30:02 +0800

    Update App.tsx

--- packages/cra-template-typescript/template/src/App.tsx
+++ packages/cra-template-typescript/template/src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,7 +25,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
